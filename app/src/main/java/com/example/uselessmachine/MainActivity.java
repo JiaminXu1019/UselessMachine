590f2d0e6b574ba59fa142f91a83f9250e6bbb6a
JiaminXu1019 <jiaminxu1019@gmail.com>
2018-08-27 09:41:26 -0700

    -Added a switch and button to layout. -Added an OnCheckedChanged Listener to the switch.

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.uselessmachine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch switchUseless;
+    private Button buttonSelfdestruct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wireWidgets();
+        setListeners();
+    }
+
+    private void setListeners(){
+        switchUseless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked)
+                {
+                    Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Off", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    private void wireWidgets() {
+        buttonSelfdestruct = findViewById(R.id.button_main_selfdestruct);
+        switchUseless = findViewById(R.id.switch_main_useless);
     }
 }
+
