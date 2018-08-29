bca45c7c92e136396ee14d1e55ef0c96c865506a
JiaminXu1019 <jiaminxu1019@gmail.com>
2018-08-29 09:40:04 -0700

    Flashing Red

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.uselessmachine;
 
+import android.graphics.Color;
 import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -13,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Switch switchUseless;
     private Button buttonSelfdestruct;
-    
+    private ConstraintLayout layout;
+
     public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -25,18 +29,23 @@ public class MainActivity extends AppCompatActivity {
         setListeners();
     }
 
-    private void setListeners(){
+    private void setListeners() {
+        buttonSelfdestruct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSelfDestructSequence();
+            }
+        });
+
         switchUseless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
 
-                StartSwitchOffTimer();
+                    StartSwitchOffTimer();
 
                     Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
+                } else {
                     Toast.makeText(MainActivity.this, "Off", Toast.LENGTH_SHORT).show();
                 }
 
@@ -44,27 +53,62 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void StartSwitchOffTimer(){
-        new CountDownTimer(5000, 100){
+    private void startSelfDestructSequence() {
+        buttonSelfdestruct.setClickable(false);
+        {
+            startSelfDestruct();
+        }
+    }
+
+    private void startSelfDestruct() {
+        new CountDownTimer(10000, 1000) {
+
             @Override
-            public void onTick(long millsUntilFinish) {
-                if(switchUseless.isChecked()){
-                    Log.d(TAG, "onTick: canceling");
-                    cancel();
+            public void onTick(long millisUntilFinished) {
+             {
+                    Toast.makeText(MainActivity.this, "Destruct in: " +  (1000 + millisUntilFinished)/1000, Toast.LENGTH_SHORT).show();
+                    if(millisUntilFinished <= 10000 && millisUntilFinished % 2 == 0 )
+                        layout.setBackgroundColor(Color.rgb(255, 80, 80));
+                    else if(millisUntilFinished <= 10000 && millisUntilFinished % 2 != 0)
+                 layout.setBackgroundColor(Color.rgb(255, 255, 255));
+             }
+            }
+
+            @Override
+            public void onFinish() {
+                finish();
+            }
+
+            }.start();
+        }
+
+        private void StartSwitchOffTimer () {
+            new CountDownTimer(5000, 100) {
+                @Override
+                public void onTick(long millsUntilFinish) {
+                    if (switchUseless.isChecked()) {
+                        Log.d(TAG, "onTick: canceling");
+                        cancel();
+
+                    }
+
                 }
 
-            }
+                @Override
+                public void onFinish() {
+                    switchUseless.setChecked(false);
+                    Log.d(TAG, "onFinish: Switch set to false");
+                }
+            }.start();
+        }
 
-            @Override
-            public void onFinish() {switchUseless.setChecked(false);
-                Log.d(TAG, "onFinish: Switch set to false");
-            }
-        }.start();
+        private void wireWidgets ()
+        {
+            buttonSelfdestruct = findViewById(R.id.button_main_selfdestruct);
+            switchUseless = findViewById(R.id.switch_main_useless);
+            layout = findViewById(R.id.layout_main_background);
+        }
+
     }
 
-    private void wireWidgets() {
-        buttonSelfdestruct = findViewById(R.id.button_main_selfdestruct);
-        switchUseless = findViewById(R.id.switch_main_useless);
-    }
-}
 
